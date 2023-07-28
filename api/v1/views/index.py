becd8df8913de068c941f791f645b2d980e33763
OmobaVII <omoba.career@gmail.com>
2023-07-28 07:19:02 -0700

    Status of your API

---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""the index file of my views"""
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """returns the status of the API"""
+    return jsonify({"status": "OK"})
