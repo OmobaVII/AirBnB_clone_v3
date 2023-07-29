3666d0b3b36627f73f183c77e7dfd6979d801913
OmobaVII <omoba.career@gmail.com>
2023-07-29 02:41:37 -0700

    User

---
+++ api/v1/views/users.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""new view for user objects that handles
+default RESTful API actions"""
+from models.user import User
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from models import storage
+
+
+@app_views.route("/users", methods=['GET'], strict_slashes=False)
+def get_user():
+    """retrives the list of all user objects"""
+    user_list = []
+    for user in storage.all(User).values():
+        user_list.append(user.to_dict())
+    return jsonify(user_list)
+
+
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+def get_userid(user_id):
+    """return a user corresponding the id"""
+    a_user = storage.get(User, user_id)
+    if a_user is None:
+        abort(404)
+
+    return jsonify(a_user.to_dict())
+
+
+@app_views.route('/users/<user_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_user(user_id):
+    """deletes a user corresponding the id"""
+    a_user = storage.get(User, user_id)
+    if a_user is None:
+        abort(404)
+    a_user.delete()
+    storage.save()
+
+    return jsonify({}), 200
+
+
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def post_user():
+    """creates a new user"""
+    if not request.get_json():
+        return jsonify({"error": "Not a JSON"}), 400
+    elif "name" not in request.get_json():
+        return jsonify({"error": "Missing name"}), 400
+    else:
+        data = request.get_json()
+        User(**data).save()
+        return jsonify(User(**data).to_dict()), 201
+
+
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+def update_user(user_id):
+    """updates a user"""
+    a_user = storage.get(User, user_id)
+    if a_user is None:
+        abort(404)
+    if not request.get_json():
+        return jsonify({"error": "Not a JSON"}), 400
+    else:
+        data = request.get_json()
+        a_user.name = data['name']
+        a_user.save()
+
+        return jsonify(a_user.to_dict()), 200
