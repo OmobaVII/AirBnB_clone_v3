5ccaba302cf910b804cb78916012a7ffbdf39ed6
KofiFloki <jeremiah.martey@yahoo.com>
2023-07-29 13:51:23 -0700

    Security improvements!

--- models/base_model.py
+++ models/base_model.py
@@ -58,7 +58,7 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self, save_to_fs=False):
+    def to_dict(self):
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
@@ -68,10 +68,10 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
-        if save_to_fs is False:
-            if "password" in new_dict:
-                del new_dict["password"]
+        if self.__class__.__name__ == "User" and not isinstance(self.storage, FileStorage):
+            del new_dict["password"]
         return new_dict
+
 
     def delete(self):
         """delete the current instance from the storage"""
