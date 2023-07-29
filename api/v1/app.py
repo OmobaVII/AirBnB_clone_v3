b44275d6f7684c964673c843ce6494eca7355c88
OmobaVII <omoba.career@gmail.com>
2023-07-29 05:17:42 -0700

    HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,7 +7,7 @@ from api.v1.views import app_views
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origines="0.0.0.0")
+CORS(app, origins="0.0.0.0")
 app.register_blueprint(app_views)
 
 
